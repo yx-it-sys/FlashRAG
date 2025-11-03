88d77ebef955ab253459f418dfbb86a619117ecc
youxu <youxu201912@163.com>
2025-11-03 21:35:20 +0800

    new

--- flashrag/pipeline/nfa_pipeline.py
+++ flashrag/pipeline/nfa_pipeline.py
@@ -1,7 +1,5 @@
 from typing import TypedDict, Annotated, List
-from langchain_core.messages import BaseMessage, AIMessage, HumanMessage, ToolMessage
-from langgraph.graph import StateGraph, END
-from langchain_core.pydantic_v1 import BaseModel, Field
+from flashrag.prompt import MMPromptTemplate
 from flashrag.pipeline import BasicMultiModalPipeline
 from flashrag.utils import get_retriever, get_generator
 import operator
@@ -10,21 +8,24 @@ import torch
 import os
 import json
 import warnings
+import tomllib
 
 class NFAPipeline(BasicMultiModalPipeline):
-    def __init__(self, config, prompt_template=None, retriever=None, generator=None):
+    def __init__(self, config, prompt_path: str, prompt_template=None, retriever=None, generator=None):
         super().__init__(config, prompt_template)
         
         self.config = config
         self.generator = get_generator(config) if generator is None else generator
         self.retriever = get_retriever(config) if retriever is None else retriever
 
+        if prompt_template is None:
+            prompt_template = MMPromptTemplate(config)
         self.prompt_template = prompt_template
 
-        self.current_state = "S_Initial"
-
-    def _state_initial(self) -> str:
+    def _state_initial(self, run_state: dict) -> str:
         return "S_Plan"
     
-    def _state_plan(self) -> str:
-        response = self.generator()
+    def _state_plan(self, run_state: dict) -> str:
+        user_prompt = self.task_prompts["plan"].format(query=run_state["current_query"])
+        response_dict = self.generator.generate()
+        return "S_Plan"
